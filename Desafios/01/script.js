5fcfa20ea43338653ac58ff72f6c49c84459ca15
PEDRO HENRIQUE GONÇALVES DORNELAS <pedrohdornelas@gmail.com>
2023-12-07 18:17:05 -0300

    Update script.js

--- Desafios/01/script.js
+++ Desafios/01/script.js
@@ -1,6 +1,7 @@
 var nome = prompt("Digite seu nome: ")
 var ano = parseInt(prompt("Em que ano você nasceu?"))
+var anoAtual= new Date().getFullYear()
 
-var idade = 2023 - ano
+var idade = anoAtual - ano
 
 console.log(`Olá, ${nome}, você possui ${idade} anos.`)
