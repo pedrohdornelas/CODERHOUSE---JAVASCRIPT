9d127bbdb588f57aabe8b86cfcdfdfdc96a76b20
PEDRO HENRIQUE GONÇALVES DORNELAS <pedrohdornelas@gmail.com>
2023-12-12 19:25:42 -0300

    Desafio 02

---
+++ Desafios/02/script.js
@@ -0,0 +1,28 @@
+function solicitarNome() {
+    var nome = prompt("Nome do aluno:");
+    alert(`Olá, ${nome}, seja bem-vindo!`);
+    return nome; 
+}
+
+var nomeAluno = solicitarNome(); 
+
+var notaMatematica = Number(prompt("Qual foi sua nota em Matemática?"));
+var notaLinguagens = Number(prompt("Qual foi sua nota em Linguagens?"));
+var notaNatureza = Number(prompt("Qual foi sua nota em Ciências da Natureza?"));
+var notaHumanas = Number(prompt("Qual foi sua nota em Humanas?"));
+
+var calcularMedia = (notaMatematica, notaLinguagens, notaNatureza, notaHumanas) => 
+  (notaMatematica + notaLinguagens + notaNatureza + notaHumanas) / 4;
+
+var media = calcularMedia(notaMatematica, notaLinguagens, notaNatureza, notaHumanas);
+
+var verificarSituacao = (media) => {
+    if (media >= 7) {
+        alert(`${nomeAluno}, você foi aprovado! Sua média final foi ${media}`);
+    } else {
+        alert(`${nomeAluno}, você foi reprovado!Sua média final foi ${media}`);
+    }
+}
+
+verificarSituacao(media);
+
